131845d76b6c9477937bfe59761035f610cb702c
Dongliang Li <dongliang14@gmail.com>
2023-03-31 11:38:17 -0700

    react router testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import CHNZheYangDeAi from '../src/songs/CHN_ZheYangDeAi'
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <h1>Hello from App</h1>
+      <Switch>
+        <Route exact path="/">
+          <CHNZheYangDeAi />
+        </Route>
+        <Route path="/test">
+          <CHNZheYangDeAi />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ frontend/src/songs/CHN_ZheYangDeAi.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CHNZheYangDeAi() {
+  return (
+    <div>
+      <h1>這樣的愛</h1>
+    </div>
+  )
+}
+
+export default CHNZheYangDeAi
