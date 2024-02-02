f967410aa11b5c8732669d03f8572b6e3435aa8a
Dongliang Li <dongliang14@gmail.com>
2024-02-02 14:37:56 -0800

    add to do list

--- frontend/src/components/Album/AlbumBasicInfo/AlbumBasicInfoData.js
+++ frontend/src/components/Album/AlbumBasicInfo/AlbumBasicInfoData.js
@@ -1,45 +1,9 @@
 export const AlbumBasicInfoData = [
   {
-    'albumName':'2021 @FJCC',
-    'year': 2021,
-    'updatedAt': 'Dec 31, 2021',
-    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV862lilSthi53e8IpvPk6zaBcSsbVxT22sZ3qFZk8ilWLfURM5cZuMqYOLNIzUkO4kBvoWe-if3jC9SB2xUNzGAyWf9YZDgJiTQzCLLYQSh4pM-IV7g=w1920-h1080'
-  },
-  {
-    'albumName':'2022 @FJCC',
-    'year': 2022,
-    'updatedAt': 'Dec 31, 2022',
-    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV86BJEy_Bt7GQQckVrIMXFIxTRrBa-iJIOQIEIoDpmwvUlROf2lIIbNWaxiJoZXd6hFBPO782v3QD4GnVaJvq_UcJFnZwyYFp9V3zGZyhL7dTzqY3so=w1920-h1080'
-  },
-  {
-    'albumName':'2022 Thanksgiving',
-    'year': 2022,
-    'updatedAt': 'Dec 31, 2022',
-    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV85NEHZG5x9LO-m8oovwkBeKiT8_BQJvGSeuLun-I3fkxxlq5Zo03tVsMFU_xQkOAHwKCM3Z6NZhqPQHZmTy6AjEfUwLIf9buIG6c4d8qfDHLbDXhOc=w1920-h1080'
-  },
-  {
-    'albumName':'2022 Christmas',
-    'year': 2022,
-    'updatedAt': 'Dec 31, 2022',
-    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV87On3NZhwQTbcLBs6l4l0YGMm0u9syd41b9-FU3YZP3l35cNU2CCaJSLzseVTI5E9Kg6cC9vwZkFEIyUH2kw_XfDTPJWk9Z3DqAhG9t7tj_Wd-TE6M=w1920-h1080'
-  },
-  {
-    'albumName':'2023 Campus Ministry',
+    'albumName':'2023 Christmas',
     'year': 2023,
     'updatedAt': 'Dec 31, 2023',
-    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV87m2X6NuLsRfhukKJN-6t61oLCa8s2D5ZPHDuKcB2VkcZq7vdXeDkjLNPAgGFdBCkrIRQXX6mThp6Z2LnZ_1OOVfA1Gvx08joRBSelf_GGnnIyrtZQ=w1920-h1080'
-  },
-  {
-    'albumName':'2023 Elderly Care Ministry',
-    'year': 2023,
-    'updatedAt': 'Dec 31, 2023',
-    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV85w4uv0z_PR4SS4x3Dpu0mhTroM2t6WTJFFfaZhW3UeAp6EV3VDRGANyMLhAL-VVLS3ITbMOHThpRZ4jr9_g44dNaL3YtQQwQ6OUOmjj0noJU9YvVo=w1920-h1080'
-  },
-  {
-    'albumName':'2023 Life Together',
-    'year': 2023,
-    'updatedAt': 'Dec 31, 2023',
-    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV84t3azys4HaG3Fwwbj-FnQhjPbGLqYTiXUb10YG5-lBXjevXfZUE-6yk78FQ0ZPSZR0QY4zi_RSSFyKozzBBfYI5Xlw5ZzODoRjCDL4V9jYubnPzj8=w1920-h1080'
+    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV87L00pYV2haptNnmfObGZKolqiyzTOgCLm9GyWRr_Hdfuaz9Z1H7SkJil2gVxXFyv6TOEHI_Eu64bCJlED9k7eblpE5AmwUEWPwX7SLd_dSsJLE36s=w1920-h1080'
   },
   {
     'albumName':'2023 Thanksgiving',
@@ -48,10 +12,45 @@ export const AlbumBasicInfoData = [
     'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV844XjH7lN3XFxyDgWk5iJ0_zkdzOORhmqFnkEupJFWAvx8L5HDBgRT_XjmSrMm9ZJL2ysDXosOwtJmUesuMeLfX8-CtAJN4VoTHCRoLwJpXHkJlTiI=w1920-h1080'
   },
   {
-    'albumName':'2023 Christmas',
+    'albumName':'2023 Life Together',
     'year': 2023,
     'updatedAt': 'Dec 31, 2023',
-    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV87L00pYV2haptNnmfObGZKolqiyzTOgCLm9GyWRr_Hdfuaz9Z1H7SkJil2gVxXFyv6TOEHI_Eu64bCJlED9k7eblpE5AmwUEWPwX7SLd_dSsJLE36s=w1920-h1080'
+    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV84t3azys4HaG3Fwwbj-FnQhjPbGLqYTiXUb10YG5-lBXjevXfZUE-6yk78FQ0ZPSZR0QY4zi_RSSFyKozzBBfYI5Xlw5ZzODoRjCDL4V9jYubnPzj8=w1920-h1080'
+  },
+  {
+    'albumName':'2023 Elderly Care Ministry',
+    'year': 2023,
+    'updatedAt': 'Dec 31, 2023',
+    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV85w4uv0z_PR4SS4x3Dpu0mhTroM2t6WTJFFfaZhW3UeAp6EV3VDRGANyMLhAL-VVLS3ITbMOHThpRZ4jr9_g44dNaL3YtQQwQ6OUOmjj0noJU9YvVo=w1920-h1080'
+  },
+  {
+    'albumName':'2023 Campus Ministry',
+    'year': 2023,
+    'updatedAt': 'Dec 31, 2023',
+    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV87m2X6NuLsRfhukKJN-6t61oLCa8s2D5ZPHDuKcB2VkcZq7vdXeDkjLNPAgGFdBCkrIRQXX6mThp6Z2LnZ_1OOVfA1Gvx08joRBSelf_GGnnIyrtZQ=w1920-h1080'
+  },
+  {
+    'albumName':'2022 Christmas',
+    'year': 2022,
+    'updatedAt': 'Dec 31, 2022',
+    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV87On3NZhwQTbcLBs6l4l0YGMm0u9syd41b9-FU3YZP3l35cNU2CCaJSLzseVTI5E9Kg6cC9vwZkFEIyUH2kw_XfDTPJWk9Z3DqAhG9t7tj_Wd-TE6M=w1920-h1080'
+  },
+  {
+    'albumName':'2022 Thanksgiving',
+    'year': 2022,
+    'updatedAt': 'Dec 31, 2022',
+    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV85NEHZG5x9LO-m8oovwkBeKiT8_BQJvGSeuLun-I3fkxxlq5Zo03tVsMFU_xQkOAHwKCM3Z6NZhqPQHZmTy6AjEfUwLIf9buIG6c4d8qfDHLbDXhOc=w1920-h1080'
+  },
+  {
+    'albumName':'2022 @FJCC',
+    'year': 2022,
+    'updatedAt': 'Dec 31, 2022',
+    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV86BJEy_Bt7GQQckVrIMXFIxTRrBa-iJIOQIEIoDpmwvUlROf2lIIbNWaxiJoZXd6hFBPO782v3QD4GnVaJvq_UcJFnZwyYFp9V3zGZyhL7dTzqY3so=w1920-h1080'
+  },
+  {
+    'albumName':'2021 @FJCC',
+    'year': 2021,
+    'updatedAt': 'Dec 31, 2021',
+    'coverImgUrl':'https://lh3.googleusercontent.com/pw/ABLVV862lilSthi53e8IpvPk6zaBcSsbVxT22sZ3qFZk8ilWLfURM5cZuMqYOLNIzUkO4kBvoWe-if3jC9SB2xUNzGAyWf9YZDgJiTQzCLLYQSh4pM-IV7g=w1920-h1080'
   }
-
 ]
