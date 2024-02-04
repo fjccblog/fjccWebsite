47742477d3c6b9213201b9d442e42672ae5ace7b
Dongliang Li <dongliang14@gmail.com>
2024-02-03 23:27:43 -0800

    animation, show new next to updated time

--- frontend/src/components/Album/Album.js
+++ frontend/src/components/Album/Album.js
@@ -2,30 +2,30 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { AlbumBasicInfoData } from './AlbumBasicInfo/AlbumBasicInfoData';
 import AlbumPlayer from './AlbumPlayer/AlbumPlayer';
-import Album2021 from './2021/Album2021';
-import Album2022 from './2022/Album2022';
-import Album2022Thanksgiving from './2022/Album2022Thanksgiving';
-import Album2022Christmas from './2022/Album2022Christmas';
-import Album2023CampusMinistry from './2023/Album2023CampusMinistry';
-import Album2023ElderCareMinistry from './2023/Album2023ElderlyCareMinistry';
-import Album2024LifeTogether from './2023/Album2023LifeTogether';
-import Album2023Thanksgiving from './2023/Album2023Thanksgiving';
-import Album2023Christmas from './2023/Album2023Christmas';
-import Album2023LifeTogether from './2023/Album2023LifeTogether';
 import './Album.css';
 
 function Album() {
+  let allAlbumInfoContainer = document.querySelector('.allAlbumInfoContainer');
+  let albumPlayerContainer = document.querySelector('.albumPlayerContainer');
 
   function filterAlbumByYear(year) {
     setCurrYearAlbums(AlbumBasicInfoData.filter((infoData)=>infoData.year === year))
+    if (allAlbumInfoContainer) allAlbumInfoContainer.style.display = "flex";
+    if (albumPlayerContainer) albumPlayerContainer.style.display = "none";
   }
 
   function updateAldumData (albumData) {
     setCurrAlbumData(albumData);
-    let allAlbumInfoContainer = document.querySelector('.allAlbumInfoContainer');
-    allAlbumInfoContainer.style.display = "none";
+    if (allAlbumInfoContainer) allAlbumInfoContainer.style.display = "none";
+    if (albumPlayerContainer) albumPlayerContainer.style.display = "block";
   }
 
+  function isAlbumRecent (currTime, albumTime) {
+    return (((currTime.getTime() - new Date(albumTime).getTime()) / 1000) < (60 * 60 * 24 * 31)) ? true : false
+
+  }
+
+  //-------------------------
   let years = [];
   let yearSet = new Set();
   let [currYearAlbums, setCurrYearAlbums] = useState([]);
@@ -43,7 +43,8 @@ function Album() {
     filterAlbumByYear(years[0]);
   }, [])
 
-
+  //------------------
+  let currTime = new Date();
 
   return (
     <div>
@@ -61,22 +62,16 @@ function Album() {
               <img src={albumInfo.coverImgUrl} className='albumInfoImg'/>
               <div className='albumInfoDescription'>
                 <span className='albumInfoDescriptionName'>{albumInfo.albumName}</span>
-                <span className='albumInfoDescriptionUpdateAt'>Last Updated: {albumInfo.updatedAt}</span>
+                <div className='albumInfoDescriptionUpdateAt'>
+                  <div>
+                    <span>Updated: {albumInfo.updatedAt} </span>
+                    {isAlbumRecent(currTime, albumInfo.updatedAt) && <span className='albumNew'>New</span>}
+                  </div>
+                </div>
               </div>
             </div>
           )})}
       </div>
-      {/* <h3>album 2023 Christmas</h3> */}
-      {/* <Album2021 /> */}
-      {/* <Album2022 /> */}
-      {/* <Album2022Thanksgiving /> */}
-      {/* <Album2022Christmas /> */}
-      {/* <Album2023CampusMinistry /> */}
-      {/* <Album2023ElderCareMinistry /> */}
-      {/* <Album2023LifeTogether /> */}
-      {/* <Album2023Thanksgiving /> */}
-      {/* <Album2023Christmas /> */}
-      {/* <AlbumPlayer data={currAlbumData} /> */}
       {currAlbumData.length !== 0 && <AlbumPlayer data={currAlbumData} />}
     </div>
   )
