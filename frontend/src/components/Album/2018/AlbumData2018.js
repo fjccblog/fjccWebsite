18de18692709296e2c28b4369be9d62daea8280c
Dongliang Li <dongliang14@gmail.com>
2024-02-20 01:04:53 -0800

    album data 2019

---
+++ frontend/src/components/Album/2018/AlbumData2018.js
@@ -0,0 +1,11 @@
+export const AlbumData2018 = [
+"https://live.staticflickr.com/4791/25692559207_427ef8c315_z.jpg",
+"https://live.staticflickr.com/4787/26691913258_fba61dc015_z.jpg",
+"https://live.staticflickr.com/4797/39852512554_a40e0c0cb7_z.jpg"
+
+
+
+
+]
+
+// https://www.flickr.com/photos/132430613@N08/albums/72157688357615070/
