6b7a57ced250fafbeaf3120fa0758b87866eb27c
Dongliang Li <dongliang14@gmail.com>
2024-02-22 01:21:25 -0800

    album link for 2012

--- tools/flickrPuppeteer/index.js
+++ tools/flickrPuppeteer/index.js
@@ -90,13 +90,67 @@ let url = 'https://www.flickr.com/photos/132430613@N08/albums/72157678808138323/
 // let url = 'https://www.flickr.com/photos/98992990@N05/albums/72157634681881076';
 // Spring 2013
 // let url = 'https://www.flickr.com/photos/98992990@N05/albums/72157634684555310';
+// Fellowship 2013
+// let url = 'https://www.flickr.com/photos/91949468@N06/albums/72157632446585883';
 
+// Helen and Zike
+// let url = 'https://www.flickr.com/photos/92484020@N02/albums/72157632585137733';
+// Helen and Zike
+// let url = 'https://www.flickr.com/photos/91949468@N06/albums/72157632506661488';
+// Christmas Party
+// let url = 'https://www.flickr.com/photos/91949468@N06/albums/72157632446583275';
+// FJCC Preparing For Christmas
+// let url = 'https://www.flickr.com/photos/91831759@N07/albums/72157632448907010';
+// 以勒堂九週年佈道和培靈雙週
+// let url = 'https://www.flickr.com/photos/88191622@N07/albums/72157631712108850';
+// Church Painting Day
+// let url = 'https://www.flickr.com/photos/86349340@N03/albums/72157631356193784';
+// 2nd Annual Neighborhood Block Party
+// let url = 'https://www.flickr.com/photos/83907351@N04/albums/72157631022804586';
+// Summer 2012
+// let url = 'https://www.flickr.com/photos/83907351@N04/albums/72157630833688622';
+// Fellowship 2012
+// let url = 'https://www.flickr.com/photos/83328473@N08/albums/72157630796057548';
+// 勁友營2012
+// let url = 'https://www.flickr.com/photos/81081856@N07/albums/72157630599514560';
+// 2012 Retreat
+// let url = 'https://www.flickr.com/photos/82724956@N03/albums/72157630599111826';
+// 2012 Retreat and Summer Outdoor Activities
+// let url = 'https://www.flickr.com/photos/82087577@N04/albums/72157630460661812';
+// 2012 Summer Retreat
+// let url = 'https://www.flickr.com/photos/82072333@N08/albums/72157630452386502';
+// Bible Seminar, Revival Meeting, Graduation
+// let url = 'https://www.flickr.com/photos/75440895@N03/albums/72157630110207704';
+// 聖經研究的樂趣研討會 2012
+// let url = '';
+// Pastor Appreciation Night
+// let url = '';
+// The Fun Continues
+// let url = 'https://www.flickr.com/photos/75440895@N03/albums/72157629525035932';
+// The Beginning of a New Year
+// let url = 'https://www.flickr.com/photos/75440895@N03/albums/72157629177764993';
 
-
-
-
-
-
+//label:2011
+//
+// let url = '';
+//
+// let url = '';
+//
+// let url = '';
+//
+// let url = '';
+//
+// let url = '';
+//
+// let url = '';
+//
+// let url = '';
+//
+// let url = '';
+//
+// let url = '';
+//
+// let url = '';
 
 
 
