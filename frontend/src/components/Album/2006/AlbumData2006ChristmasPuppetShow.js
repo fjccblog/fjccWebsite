bf55340bd609aaec38337ed6f36c7aeb44253b5e
Dongliang Li <dongliang14@gmail.com>
2024-02-26 19:39:32 -0800

    album info 2006

---
+++ frontend/src/components/Album/2006/AlbumData2006ChristmasPuppetShow.js
@@ -0,0 +1,11 @@
+export const AlbumData2006ChristmasPuppetShow = [
+  'https://live.staticflickr.com/1012/4731715825_1d46fe9039_z.jpg',
+  'https://live.staticflickr.com/1155/4732358990_1556324cf3_z.jpg',
+  'https://live.staticflickr.com/1193/4731715877_09a8f917a2_z.jpg',
+  'https://live.staticflickr.com/1157/4732358884_e65d07376e_z.jpg',
+  'https://live.staticflickr.com/1254/4732358808_3e836fef2c_c.jpg',
+  'https://live.staticflickr.com/1252/4732358670_15cda51333_z.jpg',
+  'https://live.staticflickr.com/1128/4731715193_26d9b0c06b_z.jpg',
+  'https://live.staticflickr.com/1138/4732358450_381280d727_z.jpg',
+  'https://live.staticflickr.com/1069/4732358360_d1a874c8a0_c.jpg'
+]
---
+++ frontend/src/components/Album/2008/AlbumData2008FifthAnniversaryEvangelicalMeeting.js
@@ -0,0 +1,10 @@
+export const AlbumData2008FifthAnniversaryEvangelicalMeeting = [
+  'https://live.staticflickr.com/4056/4720285954_51ae39fc8f_z.jpg',
+  'https://live.staticflickr.com/4026/4720286046_4fbb69234d_z.jpg',
+  'https://live.staticflickr.com/4030/4719635495_4e610232c7_z.jpg',
+  'https://live.staticflickr.com/1169/4726719690_5391fb828b_z.jpg',
+  'https://live.staticflickr.com/1197/4726071991_3e3d45f6ae_c.jpg',
+  'https://live.staticflickr.com/1201/4726072081_165f6a8acf_z.jpg',
+  'https://live.staticflickr.com/1376/4726072191_301e8fee69_z.jpg',
+  'https://live.staticflickr.com/1062/4726720242_58c3c5cc30_z.jpg'
+]
