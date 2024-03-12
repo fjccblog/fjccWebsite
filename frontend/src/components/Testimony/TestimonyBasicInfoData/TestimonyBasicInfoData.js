e522a8b1babc3a973cd660586a3db17a6392674a
Dongliang Li <dongliang14@gmail.com>
2024-03-11 21:35:17 -0700

    2022 testimony info

--- frontend/src/components/Testimony/TestimonyBasicInfoData/TestimonyBasicInfoData.js
+++ frontend/src/components/Testimony/TestimonyBasicInfoData/TestimonyBasicInfoData.js
@@ -1,6 +1,11 @@
 
+import { XieLiZhenSalvation } from '../2023/XieLiZhenSalvation'
+import { HuangXiMinSalvation } from '../2023/HuangXiMinSalvation'
+import { CaoRuZhenSalavation } from '../2023/CaoRuZhenSalavation'
+import { JinFengLiSalavation } from '../2023/JinFengLiSalavation'
+import { ShaoTaiGongSalavation } from '../2023/ShaoTaiGongSalavation'
 import { ShaoFenYangSalavation } from "../2024/ShaoFenYangSalavation";
-import {JunFengLeiSalavation} from "../2024/JunFengLeiSalavation";
+import { JunFengLeiSalavation } from "../2024/JunFengLeiSalavation";
 
 export const TestimonyBasicInfoData = [
   {
@@ -15,6 +20,41 @@ export const TestimonyBasicInfoData = [
     year: 2023,
     updatedAt: '2023-12-25',
     content: ShaoFenYangSalavation,
+    coverImgUrl: 'https://lh3.googleusercontent.com/pw/ABLVV858SWbRiE8PvAwmTyy2nZvXl-HUq3blWpMU0Me6vEJyu3zqbg-fnhKhOeZ0Hl1P5FW_Exa5QiXuqPbv0PkchesvOZ1--Q2Rj1GPBpx7RThYikGr-_c=w1920-h1080'
+  },
+  {
+    title: '龔紹泰得救見證',
+    year: 2022,
+    updatedAt: '2022-12-25',
+    content: ShaoTaiGongSalavation,
+    coverImgUrl: 'https://lh3.googleusercontent.com/pw/ABLVV85LFYOIjsUeYjqjR1MLV-sSxhTN5Gz0j9Ywx8w_XPqP-w_PdAgsQWFsZhlD7LcYCrU0PWv61NxMHFpkYzoMuNybCTJvLdZQngXP0Ti07TjOBU_-iVA=w1920-h1080'
+  },
+  {
+    title: '利金鳳得救見證',
+    year: 2022,
+    updatedAt: '2022-12-25',
+    content: JinFengLiSalavation,
+    coverImgUrl: 'https://lh3.googleusercontent.com/pw/ABLVV85vb1ezKjXDZGKZ4ShhajX7H3U41ITX1ZHkC-mft_7Z1OKOORRRYjFSFFmL7DN1vOV4JZ8bFC_fai2NDG2uKeSxv4kkyRrWOYUSYfzS_yNOrdgbktA=w1920-h1080'
+  },
+  {
+    title: '曹汝珍得救見證',
+    year: 2022,
+    updatedAt: '2022-12-25',
+    content: CaoRuZhenSalavation,
+    coverImgUrl: 'https://lh3.googleusercontent.com/pw/ABLVV858SWbRiE8PvAwmTyy2nZvXl-HUq3blWpMU0Me6vEJyu3zqbg-fnhKhOeZ0Hl1P5FW_Exa5QiXuqPbv0PkchesvOZ1--Q2Rj1GPBpx7RThYikGr-_c=w1920-h1080'
+  },
+  {
+    title: '黄锡民得救见证',
+    year: 2022,
+    updatedAt: '2022-12-25',
+    content: HuangXiMinSalvation,
+    coverImgUrl: 'https://lh3.googleusercontent.com/pw/ABLVV858SWbRiE8PvAwmTyy2nZvXl-HUq3blWpMU0Me6vEJyu3zqbg-fnhKhOeZ0Hl1P5FW_Exa5QiXuqPbv0PkchesvOZ1--Q2Rj1GPBpx7RThYikGr-_c=w1920-h1080'
+  },
+  {
+    title: '謝麗珍得救见证',
+    year: 2022,
+    updatedAt: '2022-12-25',
+    content: XieLiZhenSalvation,
     coverImgUrl: 'https://lh3.googleusercontent.com/pw/ABLVV858SWbRiE8PvAwmTyy2nZvXl-HUq3blWpMU0Me6vEJyu3zqbg-fnhKhOeZ0Hl1P5FW_Exa5QiXuqPbv0PkchesvOZ1--Q2Rj1GPBpx7RThYikGr-_c=w1920-h1080'
   }
 ];
