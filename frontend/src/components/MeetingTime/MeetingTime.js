a8e2e5a587d216fcb5c7e2a567eb09ba9bf255d3
Dongliang Li <dongliang14@gmail.com>
2024-06-03 23:12:03 -0700

    add meeting time

---
+++ frontend/src/components/MeetingTime/MeetingTime.js
@@ -0,0 +1,54 @@
+import React from "react";
+import './MeetingTime.css';
+
+function MeetingTime () {
+  return (
+  <div>
+    <h1>教會聚會時間</h1>
+    <img scr='https://lh3.googleusercontent.com/pw/AP1GczM3KtUEA_xG8a0LO7qFJWGNrgDDwMyHTyIQmVu_eGtNOXlDHDzZeeGgtaxo31Ti7jwsAdkFwl-JxrTLfcojHqGaWUPbHiilnH9RIPFAvNETcVgDqeE=w1920-h1080'></img>
+    <div>
+      <h3>主日崇拜 Corporate Worship Service:</h3>
+      <div><div>以勒堂</div> <div>星期日 11:00 AM - 12:15 PM</div></div>
+      <div><div>Faith Jireh Worship Service</div> <div>Sunday 11:00 AM - 12:15 PM</div></div>
+      <div><div>北門長者之家</div> <div>星期二 4:00 PM - 5:00 PM</div></div>
+      <div><div>Northgate Worship Service</div> <div>Tuesday 4:00 PM - 5:00 PM</div></div>
+      <div><div>瑪莉園&伊甸長者之家</div> <div>星期日 8:30 AM - 9:30 AM</div></div>
+      <div><div>St. Mary's Gardens & Beth Eden Worship Service</div> <div>Sunday 8:30 AM - 9:30 AM</div></div>
+    </div>
+
+    <div>
+      <h3>禱告會 Praying Meeting:</h3>
+      <div><div>教會禱告會</div> <div>星期三 8:00 PM</div></div>
+      <div><div>Church Prayer Meeting</div> <div>Wednesday 8:00 PM</div></div>
+      <div><div>教會禱告會</div> <div>星期日 12:15 PM</div></div>
+      <div><div>Church Prayer Meeting</div> <div>Sunday 12:15 PM</div></div>
+    </div>
+
+    <div>
+      <h3>基督教教育課程 Christian Education:</h3>
+      <div>今季課程 Church School Schedule</div>
+      <div><div>課程</div> <div>教師</div></div>
+      <div><div>基要真理、浸禮班</div> <div>李艷芝傳道</div></div>
+    </div>
+
+    <div>
+      <h3>信徒團契 Fellowship:</h3>
+      <div><div>青年查經團契</div> <div>星期五 8:00 PM - 10:00 PM</div></div>
+      <div><div>TGIF Bible Study Fellowship</div> <div>Friday 8:00 PM - 10:00 PM</div></div>
+      <div><div>教會愛筵及團契</div> <div>星期日 12:30 PM</div></div>
+      <div><div>Love Feast & Fellowship</div> <div>Sunday 12:30 PM</div></div>
+      <div><div>樂力校園團契</div> <div>星期三 12:00 PM - 1:00 PM</div></div>
+      <div><div>Living Strong Campus Fellowship</div> <div>Wednesday 12:00 PM - 1:00 PM</div></div>
+    </div>
+
+    <div>
+      <h3>外展事工 Outreach Ministry:</h3>
+      <div>學生事工 Student Ministry</div>
+      <div>生命麵包事工 Bethlehem Ministry</div>
+    </div>
+
+  </div>
+  )
+}
+
+export default MeetingTime;
