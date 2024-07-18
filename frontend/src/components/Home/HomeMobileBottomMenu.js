5f9b0914883da974b0b91ad3cf1f09bcaadf32fd
Dongliang Li <dongliang14@gmail.com>
2024-07-17 18:57:14 -0700

    logo rework

--- frontend/src/components/Home/HomeMobileBottomMenu.js
+++ frontend/src/components/Home/HomeMobileBottomMenu.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import OpenModalButton from '../../context/OpenModalButton';
+import NavModal from '../Navbar/NavModal';
 
 function HomeMobileBottomMenu () {
   return (
     <div className=''>
-
+      <OpenModalButton modalComponent={<NavModal />}
+      buttonText={<i className="fas fa-bars"></i>}
+      customizeStyle='menuBtn'
+      modalContentCss='modalRight'
+      modalBackgroundCss='modalBackgroundFadeIn'/>
 
     </div>
   )
