06f6458ed584da95d41f2218e8bf6e747e2baae9
Dongliang Li <dongliang14@gmail.com>
2024-07-19 13:02:14 -0700

    menu btn rework 2, need to adjust center position

--- frontend/src/components/Home/HomeMobileBottomMenu.js
+++ frontend/src/components/Home/HomeMobileBottomMenu.js
@@ -6,21 +6,21 @@ import './HomeMobileBottomMenu.css';
 function HomeMobileBottomMenu () {
   return (
     <div className='homeMobileBottomMenuContainer'>
+      <div className="menuModalBtnDiv">
+        <OpenModalButton modalComponent={<NavModal />}
+          buttonTag="div"
+          buttonText={
+            <div className="menuMainBtnDiv menuBtn">
+              <div><i className="fas fa-bars"></i></div>
+              <div>菜单</div>
+            </div>}
+          modalContentCss='modalRight'
+          modalBackgroundCss='modalBackgroundFadeIn'/>
+      </div>
       <div className="homeMenuContentDiv">
         <div className="menuBtn">Replace</div>
         <div className="menuBtn">Replace</div>
-        <div className="menuModalBtnDiv">
-          <OpenModalButton modalComponent={<NavModal />}
-            buttonTag="div"
-            buttonText={
-              <div className="menuMainBtnDiv menuBtn">
-                <div><i className="fas fa-bars"></i></div>
-                <div>菜单</div>
-              </div>}
-            modalContentCss='modalRight'
-            modalBackgroundCss='modalBackgroundFadeIn'/>
-
-        </div>
+        <div></div>
         <div className="menuBtn">
           <div><i className="fas fa-hand-holding-heart"></i></div>
           <div>奉献</div>
