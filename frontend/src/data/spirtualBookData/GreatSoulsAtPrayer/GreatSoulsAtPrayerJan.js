363b2c363cd276d4ea541020c46ec479c6e6c328
Dongliang Li <dongliang14@gmail.com>
2024-08-02 00:15:39 -0700

    add spirtual book Great Souls at Prayer tempalte data structure

---
+++ frontend/src/data/spirtualBookData/GreatSoulsAtPrayer/GreatSoulsAtPrayerJan.js
@@ -0,0 +1,332 @@
+export const GreatSoulsAtPrayerJan = {
+  1: {
+    total: 2,
+    1: {
+      "prayer": "全能的神，求禰憐憫我們，因為我們受過去的事困擾，失去信心、勇氣與盼望。求禰憐憫我們，保抱我們，使我們有真實的信心支持，深信禰是恩慈而憐憫的，我們就勇往直前，遵守禰的誡命，領取禰的豐盛，倚靠禰的憐憫，盼望永生。求主使我們無論遭遇什麼都記得有禰的引導，在禰的愛顧之下，受禰旨意支配，以至我們在最幽暗的日子中，仍能仰望禰，再有勇敢上進，有信心堅持，有盼望忍耐，始終如一。阿們！",
+      "author":"杜森"
+    },
+    2: {
+      "prayer":"主啊，禰是永遠不變的神，求禰使我們存忠實的心行經這一年，在一切的事上都在禰愛的眼光中蒙悅納。阿們！",
+      "author":"莫隆勒書（七〇〇前）"
+    }
+  },
+  2: {
+    total: 2,
+    1: {
+      "prayer": "神啊，求禰垂聽我們囚禁者的嘆息，求禰全能的力量拯救我們，脫離一切的捆鎖，使我們身心的罪過都能除去。奉我們主耶穌基督的名，阿們！",
+      "author":"羅馬聖書"
+    },
+    2: {
+      "prayer":"主我們的神！在禰翅翼的蔭下，我們總有盼望。禰要支持我們，從幼年至年老。我們的力量出於禰，才是真正的能力，不然我們自身的力量只是衰弱而已。主啊，我們睏倦的心靈要轉向禰，只靠着禰所造的事物；我們必須向著禰自己，才能重新得着真實的力量。阿們！",
+      "author":"奧古斯丁（三五四－四三〇）"
+    }
+  },
+  3: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  4: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  5: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  6: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  7: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  8: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  9: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  10: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  11: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  12: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  13: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  14: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  15: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  16: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  17: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  18: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  19: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  20: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  21: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  22: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  23: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  24: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  25: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  26: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  27: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  28: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  29: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  },
+  30: {
+    total: 2,
+    1: {
+      "prayer": "",
+      "author":""
+    },
+    2: {
+      "prayer":"",
+      "author":""
+    }
+  }
+}
