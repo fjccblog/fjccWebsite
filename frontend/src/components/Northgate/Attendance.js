414f5b68dfbec9acf9cf49767d2046fb2b8bd359
Dongliang Li <dongliang14@gmail.com>
2024-08-10 12:28:17 -0700

    useEffect on cookie change

--- frontend/src/components/Northgate/Attendance.js
+++ frontend/src/components/Northgate/Attendance.js
@@ -1,52 +1,108 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { NGPeople } from '../../data/Attendance/NGPeople';
 
 function Attendance() {
 
-  // step 1
+
+  // ----------- step 1 -----------
   // store room number (5-digit, 4 digit for room number, 1 to identify person)
   // and hour, minute, second as a string of cookie
-  // step 2
+  // -> perhaps use alphbet to shorter length
+  // set expire date in 3 months
+  // ----------- step 2 -----------
+  // for display, desture cookie into object
+  // get order/time from cookie
+  // perhaps useState for count ?
+  // ----------- step 3 -----------
   // store in Google Excels (every check-in record? or submit at the end)
   // duplicate template into new record for the day
   // check the box that correspongding to room number
   // open modal to ask for reason that can't join
 
-  // =========== features in mind
+  // ------------ features in mind -------------
   // sort by room number and sort by time
   // drag and drop for admin override (?)
-  let NGPeopleArr = Object.entries(NGPeople).sort((a,b)=>a[0] - b[0])
+  // view old cookie
 
-  const getTimeNow = () => {
-    // return time in this pattern 10:28:19 AM
-    let timeNowInMS= new Date().getTime();
-    let readableTime = new Date(timeNowInMS); //Fri Aug 09 2024 10:31:17 GMT-0700 (Pacific Daylight Time)
-    let localTime = readableTime.toLocaleString('en-US', {timeZone:"America/Los_Angeles"}) //8/9/2024, 10:28:19 AM
-    let localTimeHourMinSec = localTime.split(",")[1];
-    return localTimeHourMinSec
-  }
+  // =============== variable define ===================
+  let NGPeopleArr = Object.entries(NGPeople).sort((a,b)=>a[0] - b[0]) //[room,[CHN_name, ENG_name]]
+  const scriptUrl = process.env.REACT_APP_ATTENDANCE_SCRIPT_URL;
 
-  const submitTime = (room) => {
-    let currTime = getTimeNow()
-    // store time/order(2 digit) and room number in cookie
-    // show time / order next to name
-    // check box in excels
-  }
-
+  let [cookieVal, setCookieVal] = useState("");
+  let [cookieObj, setCookieObj] = useState({}); // {"room": [order, time]}
+  let [currDateTime, setCurrDateTime] = useState([]);
 
   const formRef = useRef(null)
 
-  const scriptUrl = process.env.REACT_APP_ATTENDANCE_SCRIPT_URL;
+
+  // ================ function definition ==============
+  const getTimeNow = () => {
+    // return time in this pattern ['8/9/2024', '10:28:19 AM']
+    let timeNowInMS= new Date().getTime();
+    let readableTime = new Date(timeNowInMS); //Fri Aug 09 2024 10:31:17 GMT-0700 (Pacific Daylight Time)
+    let localTime = readableTime.toLocaleString('en-US', {timeZone:"America/Los_Angeles"}) //8/9/2024, 10:28:19 AM
+    let localTimeArr = localTime.split(",");
+    return localTimeArr
+  }
+
+  const submitTime = (room) => {
+    // let currTime = getTimeNow()
+    // store time/order(2 digit) and room number in cookie
+    // show time / order next to name
+    // check box in excels
+    if (cookieVal.indexOf(room) === -1) {
+      let newCookie = cookieVal + room + getTimeNow()[1] + "-"
+      setCookieVal(newCookie)
+      document.cookie = `${"NG"+currDateTime[0]}=${newCookie}`;
+    }
+  }
 
   const handleSubmit = (e) =>{
-    e.preventDefault()
-    fetch(scriptUrl, {method: 'POST', body: new FormData(formRef.current)})
-    .then(res => {
-      if (res.okay)
-        console.log("SUCCESSFULLY SUBMITTED")
-    })
-    .catch(err => console.log(err))
+    // e.preventDefault()
+    // fetch(scriptUrl, {method: 'POST', body: new FormData(formRef.current)})
+    // .then(res => {
+    //   if (res.okay)
+    //     console.log("SUCCESSFULLY SUBMITTED")
+    // })
+    // .catch(err => console.log(err))
   }
+
+  // ============ useEffect ===================
+  // on render
+  useEffect(()=> {
+    setCurrDateTime(getTimeNow())
+  }, [])
+
+  useEffect(()=> {
+    // check if cookie exists
+    // if it doesn't, save empty string for cookie
+    // else construct object from cookie
+    let currCookieDate = "NG"+currDateTime[0];
+    if (document.cookie.indexOf(currCookieDate) === -1) {
+      document.cookie = `${currCookieDate}=""`
+    } else {
+      let checkInArr = currCookieDate.split("-");
+      let checkInObj = cookieObj;
+      for (let i = 0; i < checkInArr.length - 1; i++) {
+        let checkIn = checkInArr[i];
+        let room = checkIn.split(" ")[0]
+        let time = checkIn.split(" ")[1]
+        if (checkInObj[room] === undefined) {
+          checkInObj[room] = [i, time]
+        }
+      }
+      setCookieObj(checkInObj)
+    }
+  }, [currDateTime])
+
+  // decrypt cookie into object and set new cookie
+  useEffect(()=> {
+    // let todayDate = "NG" + getTimeNow()[0]
+    // document.cookie = `${todayDate}=${cookieVal}`
+    let cookieArr = cookieVal.split("-");
+    console.log(cookieArr)
+  }, [cookieVal])
+
 
   return (
     <div className='northgateContainer'>
@@ -88,6 +144,7 @@ function Attendance() {
           return (
             <div >
               {person[1].CHN_Name}
+              {cookieObj[person[0]] !== undefined ? cookieObj[person[0]][0] : "not working "}
               <button onClick={()=> submitTime(person[0])}> 签到</button>
             </div>
           )
