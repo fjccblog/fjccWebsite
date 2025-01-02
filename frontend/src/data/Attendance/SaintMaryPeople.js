1258b0bc8d17dbeac5b23c2f2d98d0dab56f842a
Dongliang Li <dongliang14@gmail.com>
2025-01-02 14:17:28 -0800

    disable mobile button highlight

---
+++ frontend/src/data/Attendance/SaintMaryPeople.js
@@ -0,0 +1,8 @@
+export const NGPeople = {
+  '02030': {"CHN_Name": "李十梅", "ENG_Name":"LI, Xi Mei"},
+
+  '99990': {"CHN_Name": "Rev. Siu", "ENG_Name":""},
+  '99991': {"CHN_Name": "Pastor Ivy", "ENG_Name":""},
+  '99992': {"CHN_Name": "Jay Lei", "ENG_Name":""},
+  '99993': {"CHN_Name": "Dongliang Li", "ENG_Name":""},
+}
